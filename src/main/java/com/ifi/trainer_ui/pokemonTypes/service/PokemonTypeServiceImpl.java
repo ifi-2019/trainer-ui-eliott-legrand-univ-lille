6cdd100f014e3af3f7ae340872d354aabdb31361
Legrand Eliott <legrande@b11p13.fil.univ-lille1.fr>
2019-11-26 10:03:55 +0100

    update tp5, PokemonTypeService

---
+++ src/main/java/com/ifi/trainer_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ifi.trainer_ui.pokemonTypes.service;
+
+import com.ifi.trainer_ui.pokemonTypes.bo.PokemonType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PokemonTypeServiceImpl implements PokemonTypeService {
+
+    private RestTemplate restTemplate;
+    private String pokeServiceUrl;
+
+    public List<PokemonType> listPokemonsTypes() {
+        // TODO
+        PokemonType[] pokeList = restTemplate.getForObject(pokeServiceUrl+"/pokemon-types/", PokemonType[].class);
+
+        return Arrays.asList(pokeList);
+    }
+
+    @Autowired
+    void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate=restTemplate;
+    }
+
+    @Value("${pokemonType.service.url}")
+    void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
+        this.pokeServiceUrl = pokemonServiceUrl;
+    }
+}
